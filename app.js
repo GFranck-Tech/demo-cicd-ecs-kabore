882df6815a6337ef40935ed6d04c4b1d44d60723
Gfranck-Tech <franckkabore394@gmail.com>
2026-07-20 21:56:30 -0200

    Initial commit - app demo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello depuis ECS Fargate ! Déploiement via pipeline CI/CD.');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`App demarree sur le port ${PORT}`);
+});
